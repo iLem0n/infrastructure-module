3b7f23e392a79945ca4b2760c8f12ff12eb2420b
Peter C. Glade (iLem0n) <peter.c.glade@googlemail.com>
2022-04-05 14:21:33 +0200

    INIT

---
+++ mcm/main.tf
@@ -0,0 +1,7 @@
+
+helm_release "saga-service-release" {
+    name        = "command-service"
+    namespace   = var.namespace_name
+    chart       = "saga-service"
+    repository  = "https://ilem0n.github.io/helm-repo/"
+}
